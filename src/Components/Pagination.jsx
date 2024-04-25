a710b67ecb9d5684ce83f7455269d4f2f652b0fa
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-25 23:53:44 +0530

    commit

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -4,7 +4,7 @@ import { AppContext } from '../Context/AppContext';
 
 
 const Pagination = () => {
-    const{page, handlePageChange, totalPages} = useContext(AppContext);
+    const {page, handlePageChange, totalPages} = useContext(AppContext);
   return (
     <div className = 'w-full flex justify-center items-center fixed bottom-0 bg-white border-2'>
         <div className='flex justify-between w-11/12 max-w-[670px] py-2'>
